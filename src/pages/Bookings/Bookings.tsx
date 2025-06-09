dd58c606a067206e008ee059789335fb998f6b40
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 14:26:01 +0700

    create Bookings page component

---
+++ src/pages/Bookings/Bookings.tsx
@@ -0,0 +1,13 @@
+import Heading from "../ui/Heading/Heading";
+import { Row } from "../ui/Row/Row";
+
+function Bookings() {
+    return (
+        <Row type="horizontal">
+            <Heading as="h1">All bookings</Heading>
+            <p>TEST</p>
+        </Row>
+    );
+}
+
+export default Bookings;
