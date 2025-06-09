0ca46fa89a58c24e04e80589df3333adf48f555f
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 14:29:01 +0700

    feat(page): create Cabins component; fix: import paths in Cabins component

---
+++ src/pages/Cabins/Cabins.tsx
@@ -0,0 +1,13 @@
+import Heading from "../../ui/Heading/Heading";
+import { Row } from "../../ui/Row/Row";
+
+function Cabins() {
+    return (
+        <Row type="horizontal">
+            <Heading as="h1">All cabins</Heading>
+            <p>TEST</p>
+        </Row>
+    );
+}
+
+export default Cabins;
