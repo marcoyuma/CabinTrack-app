3974b4f7eef4bbf32f3657d6a3709ec8d7e8d1d0
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 14:52:51 +0700

    feat: adding icons in MainNav component

--- src/ui/MainNav/MainNav.tsx
+++ src/ui/MainNav/MainNav.tsx
@@ -1,6 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { HiOutlineCalendar, HiOutlineHome } from "react-icons/hi2";
+import {
+    HiOutlineCalendarDays,
+    HiOutlineCog6Tooth,
+    HiOutlineHome,
+    HiOutlineHomeModern,
+    HiOutlineUsers,
+} from "react-icons/hi2";
 
 export const NavList = styled.ul`
     display: flex;
@@ -60,8 +66,26 @@ export const MainNav = () => {
                 </li>
                 <li>
                     <StyledNavLink to="/bookings">
-                        <HiOutlineCalendar />
-                        <span>Booking</span>
+                        <HiOutlineCalendarDays />
+                        <span>Bookings</span>
+                    </StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to="/cabins">
+                        <HiOutlineHomeModern />
+                        <span>Cabins</span>
+                    </StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to="/bookings">
+                        <HiOutlineUsers />
+                        <span>Users</span>
+                    </StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to="/bookings">
+                        <HiOutlineCog6Tooth />
+                        <span>Settings</span>
                     </StyledNavLink>
                 </li>
             </NavList>
