cdc30b6ecec099daa8aba627ae828084c9a44b1a
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 15:01:39 +0700

    fix: import path in AppLayout component

--- src/ui/AppLayout/AppLayout.tsx
+++ src/ui/AppLayout/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
 import styled from "styled-components";
-import { Sidebar } from "../SIdebar/Sidebar";
+import { Sidebar } from "../Sidebar/Sidebar";
 
 const StyledAppLayout = styled.div`
     display: grid;
