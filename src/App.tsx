dfe270a9342ebb09cda0a23bd0bd5146010cff94
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 13:35:43 +0700

    feat(router): setup main router with AppLayout and 8 child routes
    Child routes include:
    - /
    - /dashboard
    - /bookings
    - /cabins
    - /users
    - /settings
    - /account
    - /login

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,41 @@
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider,
+} from "react-router-dom";
+import Dashboard from "../../starter/temp/pages/Dashboard";
+import Account from "../../starter/temp/pages/Account";
+import Bookings from "../../starter/temp/pages/Bookings";
+import Login from "../../starter/temp/pages/Login";
+import Settings from "../../starter/temp/pages/Settings";
+import { Users } from "../../starter/temp/pages/Users";
+import PageNotFound from "../../starter/temp/pages/PageNotFound";
+import GlobalStyles from "../../starter/temp/styles/GlobalStyles";
+import { AppLayout } from "../../starter/temp/ui/AppLayout/AppLayout";
+
 export const App = () => {
-    return <div className="app"></div>;
+    const router = createBrowserRouter([
+        {
+            element: <AppLayout />,
+            children: [
+                { index: true, element: <Navigate replace to="dashboard" /> },
+                { path: "dashboard", element: <Dashboard /> },
+                { path: "account", element: <Account /> },
+                { path: "bookings", element: <Bookings /> },
+                { path: "settings", element: <Settings /> },
+                { path: "users", element: <Users /> },
+                { path: "dashboard", element: <Dashboard /> },
+                { path: "dashboard", element: <Dashboard /> },
+            ],
+        },
+        { path: "login", element: <Login /> },
+        { path: "*", element: <PageNotFound /> },
+        // path opened as soon as the app is open then directly navigate to 'dashboard' path
+    ]);
+    return (
+        <>
+            <GlobalStyles />
+            <RouterProvider router={router} />
+        </>
+    );
 };
