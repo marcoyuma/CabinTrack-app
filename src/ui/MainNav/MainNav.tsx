19172780909e9a78c8b53200bf140e6064f0573c
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 14:57:07 +0700

    fix: 'to' NavLink properties into right route

--- src/ui/MainNav/MainNav.tsx
+++ src/ui/MainNav/MainNav.tsx
@@ -77,13 +77,13 @@ export const MainNav = () => {
                     </StyledNavLink>
                 </li>
                 <li>
-                    <StyledNavLink to="/bookings">
+                    <StyledNavLink to="/users">
                         <HiOutlineUsers />
                         <span>Users</span>
                     </StyledNavLink>
                 </li>
                 <li>
-                    <StyledNavLink to="/bookings">
+                    <StyledNavLink to="/settings">
                         <HiOutlineCog6Tooth />
                         <span>Settings</span>
                     </StyledNavLink>
