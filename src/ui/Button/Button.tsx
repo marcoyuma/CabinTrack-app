851af9111870b3a3600febafb9d28e6f1ae90f1f
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 14:05:09 +0700

    feat(ui): create reusable Button component
    The Button component supports customizable styles and can be reused
    across multiple pages or features to maintain consistency.

---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,67 @@
+import styled, { css } from "styled-components";
+
+type ButtonType = {
+    size: "small" | "medium" | "large";
+    variation: "primary" | "secondary" | "danger";
+};
+const sizes = {
+    small: css`
+        font-size: 1.2rem;
+        padding: 0.4rem 0.8rem;
+        text-transform: uppercase;
+        font-weight: 600;
+        text-align: center;
+    `,
+    medium: css`
+        font-size: 1.4rem;
+        padding: 0.4rem 0.8rem;
+        font-weight: 500;
+    `,
+    large: css`
+        font-size: 1.6rem;
+        padding: 1.2rem 2.4rem;
+        font-weight: 500;
+    `,
+};
+
+const variations = {
+    primary: css`
+        color: var(--color-brand-50);
+        background-color: var(--color-brand-600);
+        &:hover {
+            background-color: var(--color-brand-700);
+        }
+    `,
+    secondary: css`
+        color: var(--color-grey-60);
+        background: var(--color-grey-0);
+        border: 1px solid var(--color-grey-200);
+        &:hover {
+            background-color: var(--color-grey-50);
+        }
+    `,
+    danger: css`
+        color: var(--color-red-100);
+        background-color: var(--color-red-700);
+
+        &:hover {
+            background-color: var(--color-red-800);
+        }
+    `,
+};
+
+/* define styled component with add-on props type 'ButtonType' */
+export const Button = styled.button<ButtonType>`
+    border: none;
+    border-radius: var(--border-radous-sm);
+    box-shadow: var(--shadow-sm);
+
+    ${(props) => sizes[props.size]}
+    ${(props) => variations[props.variation]}
+`;
+
+/* without passing 'variation' and 'size' props, these sets will be default */
+Button.defaultProps = {
+    variation: "primary",
+    size: "medium",
+};
