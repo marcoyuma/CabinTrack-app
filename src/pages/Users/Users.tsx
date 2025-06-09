5b1fe1ba6c9f0dcf983545475b812d243f12d9fd
Marco Yuma <marco.yumarafii46@gmail.com>
2025-06-09 14:36:50 +0700

    fix: import path in Users component

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,4 +1,4 @@
-import Heading from "../ui/Heading/Heading";
+import Heading from "../../ui/Heading/Heading";
 
 export const Users = () => {
     return <Heading as="h1">Create a new user</Heading>;
