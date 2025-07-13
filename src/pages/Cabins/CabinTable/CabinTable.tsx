2e78f5fdb1d4f538003add989b7980356aa1d36e
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-14 01:12:27 +0700

    refactor: move cabin fetching logic from CabinTable to useCabins hook

---
+++ src/pages/Cabins/CabinTable/CabinTable.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import Spinner from "../../../ui/Spinner/Spinner";
+import { CabinRow } from "../CabinRow/CabinRow";
+import { useCabins } from "../hooks/useCabins";
+
+const Table = styled.div`
+    border: 1px solid var(--color-grey-200);
+
+    font-size: 1.4rem;
+    background-color: var(--color-grey-0);
+    border-radius: 7px;
+    overflow: hidden;
+`;
+
+const TableHeader = styled.header`
+    display: grid;
+    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+    column-gap: 2.4rem;
+    align-items: center;
+
+    background-color: var(--color-grey-50);
+    border-bottom: 1px solid var(--color-grey-100);
+    text-transform: uppercase;
+    letter-spacing: 0.4px;
+    font-weight: 600;
+    color: var(--color-grey-600);
+    padding: 1.6rem 2.4rem;
+`;
+
+export const CabinTable = () => {
+    // destructuring the cabins data from custom hooks that calls 'getCabins' api
+    const { isPending, cabins } = useCabins();
+    console.log(isPending, cabins);
+    if (isPending) return <Spinner />;
+    return (
+        // wrap the content and make sure browser knows the rule cuz we styled it with div
+        <Table role="table">
+            {/* wrap child into 6 columns */}
+            <TableHeader role="row">
+                <div></div>
+                <div>Cabin</div>
+                <div>Capacity</div>
+                <div>Price</div>
+                <div>Discount</div>
+                <div></div>
+            </TableHeader>
+            {cabins?.map((cabin) => (
+                <CabinRow cabin={cabin} key={cabin.id} />
+            ))}
+        </Table>
+    );
+};
