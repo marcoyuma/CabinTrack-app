672d686c1af30871571aefc6eaeb3c807b644e39
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-16 19:39:35 +0700

    refactor: restructure Settings component to include Row layout for better organization

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,7 +1,14 @@
 import Heading from "../../ui/Heading/Heading";
+import { Row } from "../../ui/Row/Row";
+import UpdateSettingsForm from "./UpdateSettingsForm/UpdateSettingsForm";
 
 function Settings() {
-    return <Heading as="h1">Update hotel settings</Heading>;
+    return (
+        <Row type="vertical">
+            <Heading as="h1">Update hotel settings</Heading>
+            <UpdateSettingsForm />
+        </Row>
+    );
 }
 
 export default Settings;
