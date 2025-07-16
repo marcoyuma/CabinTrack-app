1380f33c3102305768cac030f4e69d51d82b898d
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-16 19:27:04 +0700

    feat: integrate QueryClient and Toaster into App component for enhanced state management and notifications

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 export const App = () => {
+    // define react-query instance
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                //  staleTime: 60 * 1000
+                staleTime: 0,
+            },
+        },
+    });
+
     const router = createBrowserRouter([
         {
             element: <AppLayout />,
@@ -37,9 +47,29 @@ export const App = () => {
         // path opened as soon as the app is open then directly navigate to 'dashboard' path
     ]);
     return (
-        <>
+        // wrap up our provider with query client provider
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <GlobalStyles />
             <RouterProvider router={router} />
-        </>
+            <Toaster
+                position="top-center"
+                gutter={12}
+                containerStyle={{ margin: "8px" }}
+                toastOptions={{
+                    success: {
+                        duration: 3000,
+                    },
+                    error: { duration: 5000 },
+                    style: {
+                        fontSize: "16px",
+                        maxWidth: "500px",
+                        padding: "16px 24px",
+                        backgroundColor: "var(--color-grey-0)",
+                        color: "var(--color-grey-700)",
+                    },
+                }}
+            />
+        </QueryClientProvider>
     );
 };
