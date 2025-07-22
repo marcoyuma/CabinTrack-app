3d2592503edb36bfc0b816cf8c949e19619e78e3
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-23 00:29:54 +0700

    fix: update FormType to include 'regular' and adjust styling logic for better clarity

--- src/ui/Form/Form.tsx
+++ src/ui/Form/Form.tsx
@@ -1,11 +1,17 @@
 import styled, { css } from "styled-components";
 
 type FormType = {
-    type?: "modal" | "";
+    type?: "modal" | "regular";
 };
+
+// This component is a styled form that can be used for both regular forms and modal forms.
+// It accepts a 'type' prop to determine its styling.
+// If the type is 'regular', it will have padding and a box style.
+// If the type is 'modal', it will have a specific width for modal forms.
+// The default type is 'regular'.
 const Form = styled.form<FormType>`
     ${(props) =>
-        props.type !== "modal" &&
+        props.type === "regular" &&
         css`
             padding: 2.4rem 4rem;
 
@@ -20,6 +26,9 @@ const Form = styled.form<FormType>`
         css`
             width: 80rem;
         `}
+    
+    /* way to define default props logic  */
+    ${(props) => props.type || "regular"}
     
   overflow: hidden;
     font-size: 1.4rem;
