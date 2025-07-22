d406ee1a887e61e9612681e7da865baf85430252
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-22 22:18:15 +0700

    refactor: remove unused Cabin components and related hooks for cleaner codebase

--- src/pages/Cabins/types/type.ts
+++
@@ -1,9 +0,0 @@
-export type FormDataType = {
-    name: string | undefined;
-    maxCapacity: string | undefined;
-    regularPrice: string | undefined;
-    discount: string | undefined;
-    description: string | undefined;
-    // type for default file input
-    image: FileList;
-};
