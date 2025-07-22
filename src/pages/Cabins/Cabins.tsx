db1b96e36b84fd7f616bff8aea89d43a8a6450df
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-22 22:31:02 +0700

    fix: add missing import for AddCabin component in Cabins page

--- src/pages/Cabins/Cabins.tsx
+++ src/pages/Cabins/Cabins.tsx
@@ -1,6 +1,7 @@
 import Heading from "../../ui/Heading/Heading";
 import { Row } from "../../ui/Row/Row";
 import { CabinTable } from "../../features/cabins/CabinTable/CabinTable";
+import { AddCabin } from "../../features/cabins/AddCabin/AddCabin";
 
 function Cabins() {
     return (
@@ -11,6 +12,7 @@ function Cabins() {
             </Row>
             <Row>
                 <CabinTable />
+                <AddCabin/>
             </Row>
         </>
     );
