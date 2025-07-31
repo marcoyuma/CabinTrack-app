7a84005eb6cdb6528db8c8c4a7b544da1364c5c9
Marco Yuma <marco.yumarafii46@gmail.com>
2025-07-31 10:50:43 +0700

    feat: creating CabinTable for displaying table of cabin content with compound component pattern

--- src/features/cabins/CabinTable/CabinTable.tsx
+++ src/features/cabins/CabinTable/CabinTable.tsx
@@ -15,32 +15,36 @@ import { Menus } from "../../../ui/Menus/Menus";
 export const CabinTable = () => {
     // destructuring the cabins data from custom hooks that calls 'getCabins' api
     const { isPending, cabins } = useCabins();
-    console.log(isPending, cabins);
 
     // early validation while data still on fetching, then the spinner rendered
     if (isPending) return <Spinner />;
     return (
-        // using Table component to wrap the header and rows. passing the 'columns' prop to define the grid template columns
-        // this allows child components to access the 'columns' prop via context and render the header and rows accordingly
-        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-            {/* wrap child into 6 columns */}
-            <Table.Header>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
+        // using Menus component to wrap the Table component, so CabinRow component can use the value from menus context as child
+        <Menus>
+            {/* using Table component to wrap the header and rows. passing the 'columns' prop to define the grid template columns  */}
+            {/* this allows child components to access the 'columns' prop via context and render the header and rows accordingly */}
+            <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+                {/* wrap child into 6 columns */}
+                <Table.Header>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
 
-            {/* using Table.Body to render the cabins data */}
-            {/* passing the cabins data to render the CabinRow component for each cabin */}
-            <Table.Body
-                // passing the cabin data to the CabinRow component
-                data={cabins ?? []}
-                // using the render prop to render the CabinRow component for each cabin
-                render={(cabin) => <CabinRow cabin={cabin} key={cabin.name} />}
-            />
-        </Table>
+                {/* using Table.Body to render the cabins data */}
+                {/* passing the cabins data to render the CabinRow component for each cabin */}
+                <Table.Body
+                    // passing the cabin data to the CabinRow component
+                    data={cabins ?? []}
+                    // using the render prop to render the CabinRow component for each cabin
+                    render={(cabin) => (
+                        <CabinRow cabin={cabin} key={cabin.name} />
+                    )}
+                />
+            </Table>
+        </Menus>
     );
 };
