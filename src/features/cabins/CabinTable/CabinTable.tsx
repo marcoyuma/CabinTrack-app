218a278763eb802a633a36d144f8b39c680b6c17
Marco Yuma <marco.yumarafii46@gmail.com>
2025-08-23 21:02:58 +0700

    feat: implement dynamic sorting for cabins based on URL parameters

--- src/features/cabins/CabinTable/CabinTable.tsx
+++ src/features/cabins/CabinTable/CabinTable.tsx
@@ -37,6 +37,62 @@ export const CabinTable = () => {
         );
     }
 
+    // sort cabin data
+    // get value from params using 'useURL' by defining query key name
+    const { valueFromParams: sortByValueFromParams } = useURL("sortBy");
+    console.log(sortByValueFromParams);
+
+    const sortedValue = sortByValueFromParams || "name-asc";
+
+    // sort cabins dynamically
+    const sortCabin = (
+        // cabins is cabins data array and type of 'filteredCabinsByDiscount'
+        cabins: typeof filteredCabinsByDiscount,
+
+        // base string to sort cabins
+        sortBy: string
+    ) => {
+        // early return if empty array received
+        if (!cabins) return [];
+
+        // desctructure that two splitted elements depends on '-' splitter. returned two length array
+        // Type assertion: force split returned value become tuple [string, "asc" | "desc"] for type safety
+        const [field, direction] = sortBy.split("-") as [
+            string,
+            "asc" | "desc"
+        ];
+
+        // return sorted shallow copy of 'cabins' array
+        return [...cabins].sort((a, b) => {
+            const aValue = a[field as keyof typeof a];
+            const bValue = b[field as keyof typeof b];
+
+            // sort undefined and null value to back of the list
+            if (!aValue) return 1;
+            if (!bValue) return -1;
+
+            // validation if type of value is string
+            if (typeof aValue === "string" && typeof bValue === "string") {
+                // return sorted value based on ternary 'direction' condition
+                return direction === "asc"
+                    ? aValue.localeCompare(bValue)
+                    : bValue.localeCompare(aValue);
+            }
+
+            // validation type of value is number
+            if (typeof aValue === "number" && typeof bValue === "number") {
+                // return sorted value conditionally
+                return direction === "asc" ? aValue - bValue : bValue - aValue;
+            }
+
+            // value does not contain either string or number and there's no returned 'cabins' array
+            return 0;
+        });
+    };
+
+    // call 'sortCabin'. 'filteredCabinsByDiscount' array from previous filter based on 'discount' url key and 'sortedValue' string based on 'sortBy' url key
+    const sortedCabins = sortCabin(filteredCabinsByDiscount, sortedValue);
+
     // early validation while data still on fetching, then the spinner rendered
     if (isPending) return <Spinner />;
     return (
@@ -58,8 +114,8 @@ export const CabinTable = () => {
                 {/* using Table.Body to render the cabins data */}
                 {/* passing the cabins data to render the CabinRow component for each cabin */}
                 <Table.Body
-                    // passing the cabin data to the CabinRow component
-                    data={filteredCabins ?? []}
+                    // passing the filtered and sorted cabin data to the CabinRow component
+                    data={sortedCabins ?? []}
                     // using the render prop to render the CabinRow component for each cabin
                     render={(cabin) => (
                         <CabinRow cabin={cabin} key={cabin.name} />
