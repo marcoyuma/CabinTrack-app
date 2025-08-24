9e41c23602dd312fd5a238589185cf93ab4f7a02
Marco Yuma <marco.yumarafii46@gmail.com>
2025-08-25 06:03:47 +0700

    feat: wrap BookingTable component in a fragment for improved structure

--- src/pages/Bookings/Bookings.tsx
+++ src/pages/Bookings/Bookings.tsx
@@ -1,12 +1,16 @@
+import BookingTable from "../../features/bookings/BookingTable";
 import Heading from "../../ui/Heading/Heading";
 import { Row } from "../../ui/Row/Row";
 
 function Bookings() {
     return (
+        <>
         <Row type="horizontal">
             <Heading as="h1">All bookings</Heading>
             <p>TEST</p>
         </Row>
+            <BookingTable />
+        </>
     );
 }
 
