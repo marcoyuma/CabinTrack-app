2afbb4dd9e3f49038f2d38e406847a460316106f
Marco Yuma <marco.yumarafii46@gmail.com>
2025-08-25 13:28:00 +0700

    feat: add BookingRow component for displaying booking details in a table format

---
+++ src/features/bookings/BookingRow/BookingRow.tsx
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+import { format, isToday } from "date-fns";
+
+import { Table } from "../../../ui/Table/Table";
+import Tag from "../../../ui/Tag/Tag";
+import { BookingType } from "../types/type";
+import { GuestType } from "../../guests/types/type";
+import { CabinType } from "../../cabins/types/type";
+
+import {
+    formatCurrency,
+    formatDistanceFromNow,
+    sanitizeNull,
+} from "../../../utils/helpers";
+
+// rejoin 3 table types
+type Booking = BookingType & {
+    guests: GuestType;
+    cabins: CabinType;
+};
+
+const Cabin = styled.div`
+    font-size: 1.6rem;
+    font-weight: 600;
+    color: var(--color-grey-600);
+    font-family: "Sono";
+`;
+
+const Stacked = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.2rem;
+
+    & span:first-child {
+        font-weight: 500;
+    }
+
+    & span:last-child {
+        color: var(--color-grey-500);
+        font-size: 1.2rem;
+    }
+`;
+
+const Amount = styled.div`
+    font-family: "Sono";
+    font-weight: 500;
+`;
+
+function BookingRow({
+    booking: {
+        id: bookingId,
+        created_at,
+        startDate,
+        endDate,
+        numNights,
+        numGuests,
+        totalPrice,
+        status: bookingStatus,
+        guests: { fullName: guestName, email },
+        cabins: { name: cabinName },
+    },
+}: {
+    booking: Booking;
+}) {
+    const statusToTagName = {
+        unconfirmed: "blue",
+        "checked-in": "green",
+        "checked-out": "silver",
+    };
+
+    // immediately invoke function to clean up 'status' as key for 'statusToTagName'
+    const status = ((): keyof typeof statusToTagName => {
+        const normalize = sanitizeNull(bookingStatus);
+        return normalize && normalize in statusToTagName
+            ? (normalize as keyof typeof statusToTagName)
+            : "unconfirmed";
+    })();
+
+    return (
+        <Table.Row>
+            <Cabin>{cabinName}</Cabin>
+
+            <Stacked>
+                <span>{guestName}</span>
+                <span>{email}</span>
+            </Stacked>
+
+            <Stacked>
+                <span>
+                    {isToday(new Date(startDate ?? 0))
+                        ? "Today"
+                        : formatDistanceFromNow(startDate)}{" "}
+                    &rarr; {numNights} night stay
+                </span>
+                <span>
+                    {format(new Date(startDate ?? 0), "MMM dd yyyy")} &mdash;{" "}
+                    {format(new Date(endDate ?? 0), "MMM dd yyyy")}
+                </span>
+            </Stacked>
+
+            <Tag type={statusToTagName[status]}>
+                {status?.replace("-", " ")}
+            </Tag>
+
+            <Amount>{formatCurrency(totalPrice)}</Amount>
+        </Table.Row>
+    );
+}
+
+export default BookingRow;
