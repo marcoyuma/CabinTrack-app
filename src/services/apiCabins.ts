5d923c93012db03c23c22f94198fe5017198cdc5
Marco Yuma <marco.yumarafii46@gmail.com>
2025-08-27 20:02:58 +0700

    feat: remove unused getCabins function to clean up code

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -13,18 +13,6 @@ export interface NewCabin {
     image: File | string;
 }
 
-// define get all data from 'cabins' table database
-export const getCabins = async () => {
-    // destructure the data using 'supabase' client instance from 'cabins' table
-    const { data, error } = await supabase.from("cabins").select("*");
-    if (error) {
-        console.error(error);
-        throw new Error("Cabins could not be loaded");
-    }
-
-    return data;
-};
-
 // delete cabin based on id using supabase client instance from 'cabins'
 export const deleteCabin = async (cabin: Cabin) => {
     // delete cabin data from 'cabins' table
