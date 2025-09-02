1e6fcccf06d9035c2b70e20e958cc3a45fa13bd0
Marco Yuma <marco.yumarafii46@gmail.com>
2025-09-02 20:40:09 +0700

    fix: import mutation function from createCabinApi as createCabin for mutation function

--- src/features/cabins/hooks/useCreateCabin.ts
+++ src/features/cabins/hooks/useCreateCabin.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createCabin as createCabinApi } from "../../../services/apiCabins";
+import { createCabin as createCabinApi } from "../../../services/apiCabins/createCabin";
 import toast from "react-hot-toast";
 
 export const useCreateCabin = () => {
