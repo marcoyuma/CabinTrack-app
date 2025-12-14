b111f143104e414d7219a95121f36373479b91f0
Marco Yuma <marco.yumarafii46@gmail.com>
2025-12-14 21:53:41 +0700

    style: format LoginLayout styled component for consistency

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,13 +5,13 @@ import { Heading } from "../../ui/Heading/Heading";
 import { LoginForm } from "../../features/authentication/LoginForm";
 
 const LoginLayout = styled.main`
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 48rem;
-  align-content: center;
-  justify-content: center;
-  gap: 3.2rem;
-  background-color: var(--color-grey-50);
+    min-height: 100vh;
+    display: grid;
+    grid-template-columns: 48rem;
+    align-content: center;
+    justify-content: center;
+    gap: 3.2rem;
+    background-color: var(--color-grey-50);
 `;
 
 export const Login = () => {
