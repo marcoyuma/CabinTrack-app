4c07a85467b54c0b11a9611de50bee7932cce4a5
Marco Yuma <marco.yumarafii46@gmail.com>
2025-12-17 20:11:19 +0700

    feat: add SignupForm component with form handling and validation

---
+++ src/features/authentication/components/SignupForm.tsx
@@ -0,0 +1,73 @@
+// Email regex: /\S+@\S+\.\S+/
+
+import { FieldErrors, SubmitHandler, useForm } from "react-hook-form";
+import { Button } from "../../../ui/Button/Button";
+import { Form } from "../../../ui/Form/Form";
+import { FormRow } from "../../../ui/FormRow/FormRow";
+import { Input } from "../../../ui/Input/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signupFormSchema, SignupSchemaType } from "../types/auth.schema";
+import { useSignup } from "../hooks/useSignUp";
+import { Spinner } from "../../../ui/Spinner/Spinner";
+
+function SignupForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ resolver: zodResolver(signupFormSchema) });
+
+    const { signup, isSigningUp } = useSignup();
+
+    const onSubmit: SubmitHandler<SignupSchemaType> = (data) => {
+        console.log(data);
+
+        console.log(`signup: ${data}`);
+        signup({ email: data.email, password: data.password });
+    };
+    const onError = (e: FieldErrors) => {
+        console.error(e);
+    };
+
+    if (isSigningUp) return <Spinner />;
+
+    return (
+        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+            <FormRow label="Full name" error={errors.fullName?.message}>
+                <Input type="text" id="fullName" {...register("fullName")} />
+            </FormRow>
+            <FormRow label="Email address" error={errors.email?.message}>
+                <Input type="email" id="email" {...register("email")} />
+            </FormRow>
+            <FormRow
+                label="Password (min 8 characters)"
+                error={errors.password?.message}
+            >
+                <Input
+                    type="password"
+                    id="password"
+                    {...register("password")}
+                />
+            </FormRow>
+            <FormRow
+                label="Repeat password"
+                error={errors.passwordConfirm?.message}
+            >
+                <Input
+                    type="password"
+                    id="passwordConfirm"
+                    {...register("passwordConfirm")}
+                />
+            </FormRow>
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button variation="secondary" type="reset">
+                    Cancel
+                </Button>
+                <Button>Create new user</Button>
+            </FormRow>
+        </Form>
+    );
+}
+
+export default SignupForm;
