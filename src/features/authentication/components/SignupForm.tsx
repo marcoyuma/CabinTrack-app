fcb9746395d17a44bb15c680493cda6d51cd0efd
Marco Yuma <marco.yumarafii46@gmail.com>
2025-12-30 12:31:50 +0700

    feat: enhance SignupForm to include fullName and password confirmation fields

--- src/features/authentication/components/SignupForm.tsx
+++ src/features/authentication/components/SignupForm.tsx
@@ -4,44 +4,72 @@ import { Form } from "../../../ui/Form/Form";
 import { FormRow } from "../../../ui/FormRow/FormRow";
 import { Input } from "../../../ui/Input/Input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signupFormSchema, SignupSchemaType } from "../types/auth.schema";
-import { useSignup } from "../hooks/useSignUp";
-import { Spinner } from "../../../ui/Spinner/Spinner";
+import {
+    signupFormSchema,
+    SignupFormSchema,
+} from "../types/auth.signupForm.schema";
+import { useSignup } from "../hooks/useSignup";
 
 function SignupForm() {
     const {
         register,
         handleSubmit,
         formState: { errors },
+        reset,
     } = useForm({ resolver: zodResolver(signupFormSchema) });
 
     const { signup, isSigningUp } = useSignup();
 
-    const onSubmit: SubmitHandler<SignupSchemaType> = (data) => {
-        console.log(data);
-
-        console.log(`signup: ${data}`);
-        signup({ email: data.email, password: data.password });
+    const onSubmit: SubmitHandler<SignupFormSchema> = ({
+        fullName,
+        email,
+        password,
+    }) => {
+        console.log(`signup: 
+            fullName: ${fullName}, 
+            email:    ${email},
+            password: ${password}`);
+        signup(
+            {
+                fullName: fullName,
+                email: email,
+                password: password,
+            },
+            {
+                onSettled: () => {
+                    reset();
+                },
+            }
+        );
     };
     const onError = (e: FieldErrors) => {
         console.error(e);
     };
 
-    if (isSigningUp) return <Spinner />;
-
     return (
         <Form onSubmit={handleSubmit(onSubmit, onError)}>
             <FormRow label="Full name" error={errors.fullName?.message}>
-                <Input type="text" id="fullName" {...register("fullName")} />
+                <Input
+                    disabled={isSigningUp}
+                    type="text"
+                    id="fullName"
+                    {...register("fullName")}
+                />
             </FormRow>
             <FormRow label="Email address" error={errors.email?.message}>
-                <Input type="email" id="email" {...register("email")} />
+                <Input
+                    disabled={isSigningUp}
+                    type="email"
+                    id="email"
+                    {...register("email")}
+                />
             </FormRow>
             <FormRow
                 label="Password (min 8 characters)"
                 error={errors.password?.message}
             >
                 <Input
+                    disabled={isSigningUp}
                     type="password"
                     id="password"
                     {...register("password")}
@@ -52,6 +80,7 @@ function SignupForm() {
                 error={errors.passwordConfirm?.message}
             >
                 <Input
+                    disabled={isSigningUp}
                     type="password"
                     id="passwordConfirm"
                     {...register("passwordConfirm")}
