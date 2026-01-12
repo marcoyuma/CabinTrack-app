ab32f2b9a5768d546629a5b4929d26729a9581d7
Marco Yuma <marco.yumarafii46@gmail.com>
2026-01-12 22:42:02 +0700

    feat: implement useTodayActivity React Query hook

--- src/features/check-in-out/hooks/useTodayActivity.ts
+++ src/features/check-in-out/hooks/useTodayActivity.ts
@@ -1,6 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getStaysTodayActivity } from "../../../services/apiBookings";
 
+/**
+ * Custom React Query hook to fetch and manage today's check-in and check-out stay activities
+ *
+ * @returns An object containing today's stay activities data, loading state, and error state
+ */
 export function useTodayActivity() {
     const {
         data: todayStayActivities,
