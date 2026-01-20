d36f7a57de06408bd7af73b3dd15d9ad324c907e
Marco Yuma <marco.yumarafii46@gmail.com>
2026-01-20 15:54:44 +0700

    chore: update CheckinBooking import path

--- src/pages/Checkin/Checkin.tsx
+++ src/pages/Checkin/Checkin.tsx
@@ -1,5 +1,8 @@
-import { CheckinBooking } from "../../features/check-in-out/CheckinBooking";
+import { CheckinBooking } from "../../features/check-in-out/components/CheckinBooking";
 
+/**
+ * Page component that renders the check-in booking flow
+ */
 export const Checkin = () => {
     return <CheckinBooking />;
 };
